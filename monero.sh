ff77927eb9ffac2342826422c0e2d876edb7038f
jung-ctrl <85089309+jung-ctrl@users.noreply.github.com>
2021-05-31 14:22:34 +0700

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -1,2 +1,8 @@
-sudo apt update && sudo apt install screen -y && 
-wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz && ls && tar xf xmrig-6.10.0-linux-x64.tar.gz && ls && ./xmrig-6.10.0/xmrig -o sg.monero.herominers.com:10191 -u 8AoaWf4y1ME9yAkym1mLaMMXFPAAnWs8C1a3eoZjWLV9R7JGLWro42Q97g9FvHhsAbXjPrC1Ye7WiTXb1hLtnV6B37tnEbc.7000 -p Oskaybaruss -a rx/0 -k 
+#monero.sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz 
+ls
+tar xf xmrig-6.10.0-linux-x64.tar.gz 
+ls 
+./xmrig-6.10.0/xmrig -o sg.monero.herominers.com:10191 -u 8AoaWf4y1ME9yAkym1mLaMMXFPAAnWs8C1a3eoZjWLV9R7JGLWro42Q97g9FvHhsAbXjPrC1Ye7WiTXb1hLtnV6B37tnEbc.7000 -p Oskaybaruss -a rx/0 -k 
