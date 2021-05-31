e02bcee5fcb271710c17f68be4fca300404ded61
jung-ctrl <85089309+jung-ctrl@users.noreply.github.com>
2021-05-31 09:49:39 +0700

    Create scalacoin.sh

---
+++ scalacoin.sh
@@ -0,0 +1,5 @@
+#scalacoin.sh
+sudo apt update 
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
+unzip XLArig-v5.2.2-linux-x86_64.zip
+./xlarig -o scala.herominers.com:10131 -u Ssy2Hx4cjbGezb3qf1mzUx77ppWvNVAvhKBxXZaqqSZg1WkRdB1ztCqfemfVa92mWQBvrBcUhwKp2BveyWMC8uWy5VnUKr1ERW -p surah-a panthera -k -t2
